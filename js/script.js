7d1a57df3ea1334b7291e47f09fa195a527e5c80
Claire Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-12-05 14:04:17 -0500

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 202X Claire Bedrossian All rights reserved
+// Copyright (c) 2022 Claire Bedrossian All rights reserved
 //
 // Created by: Claire Bedrossian
-// Created on: XXX 202X
+// Created on: Dec 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,14 +10,31 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-07-JS/sw.js", {
+    scope: "/ICS2O-Unit5-07-JS/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function does multiplication.
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  var numN = parseFloat(document.getElementById("integer-n").value)
+  var product = 0
+  var counter = 0
+
+  while (counter < Math.abs(numB)) {
+    counter++
+    product = product + Math.abs(numA)
+  }
+
+  if ((numA < 0 && numB > 0) || (numB < 0 && numA > 0))
+    document.getElementById("multiplication-product").innerHTML =
+      "The product is equal to - " + product + "."
+  else
+    document.getElementById("multiplication-product").innerHTML =
+      "The product is equal to " + product + "."
+}
+for (let i = 0; i < 5; i++) {
+  document.getElementById("multiplication-product").innerHTML = "The number is " + product + "<br>";
 }
